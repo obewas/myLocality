2ab248bee0e3a7ab389c1c0f5be049072cc48429
obewas <obewas1202@gmail.com>
2021-07-24 11:45:28 +0300

    moved login and logout

--- votes/urls.py
+++ votes/urls.py
@@ -5,11 +5,10 @@ from . import views
 #creation of project urls
 
 urlpatterns = [
-path("accounts/", include("django.contrib.auth.urls")),
+
 path('create_neighbourhood', views.create_neighbourhood, name='create_neighbourhood'),
 path('list', views.project_list, name='project-list'),
 path('edit/<int:pk>/', views.edit_profile, name='profile-edit'),
-path('register/', views.register, name="register"),
 path('', views.view_profile, name='profile'),
 path('upload/', views.upload, name='upload'),
 
