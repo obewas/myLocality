dc99609c577e949c88fe904f53bd01482a50a666
obewas <obewas1202@gmail.com>
2021-07-24 18:01:24 +0300

    created post crud

--- posts/urls.py
+++ posts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (
+	PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+	)
 from . import views
 urlpatterns = [
 path('post', PostListView.as_view(), name='post-home'),
 path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+path('post/new/', PostCreateView.as_view(), name='post-create'),
+path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+	ListView, DetailView, CreateView, UpdateView, DeleteView
+	)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 from django.contrib.auth.decorators import login_required
 # Create your views here.
@@ -10,15 +13,58 @@ def home(request):
 	context = {
 	'posts': posts
 	}
-	return render(request, 'post_home.html', context) 
+	return render(request, 'posts/post_home.html', context) 
 
 class PostListView(ListView):
 	model = Post
-	template_name = 'post_home.html'
+	template_name = 'posts/post_home.html'
 	context_object_name = 'posts'
 	ordering = ['-date_posted']
 
 
 class PostDetailView(DetailView):
 	model = Post
-	template_name = 'post_detail.html'
+	template_name = 'posts/post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	template_name = 'posts/post_form.html'
+	fields = '__all__' 
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = '__all__'
+	template_name = 'posts/post_form.html'
+	
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)	 
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+ 	model = Post
+ 	success_url = 'post-home'
+
+ 	def test_func(self):
+ 		post = self.get_object()
+ 		if self.request.user == post.author:
+ 			return True
+ 		return False
+
+
+
+
+
+
+
